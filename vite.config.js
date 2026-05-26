f9b6d8ea77eb62420e81aa0ad83081bbe61ae8a4
LightMoving <152558838+LightMoving@users.noreply.github.com>
2026-05-25 21:49:33 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
-// ============================================
-// FILE: vite.config.js
-// ============================================
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
